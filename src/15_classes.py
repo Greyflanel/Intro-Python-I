9c7376e325b06fe932371abb700362ae7b676060
Greyflanel <richardlovelace811@gmail.com>
2019-12-03 17:47:11 -0600

    Completed all but 14_cal and 15_classes.

--- src/15_classes.py
+++ src/15_classes.py
@@ -1,22 +1,39 @@
+# TabNine::sem
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
 
 # YOUR CODE HERE
-        
+class LatLon:
+    def _init_(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
 # YOUR CODE HERE
+class Waypoint(LatLon):
+    def _init_(self, name,  lat, lon):
+        super()._init_(lat, lon)
+        self.name = name
 
+def __str__(self):
+        return 'Waypoint of %s: Lat %s and Lon %s' % (self.name, self.lat, self.lon)
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
+class Geocache(Waypoint):
+    def _init_(self, name, difficulty, size, lat , lon):
+        super()._init_(name, lat, lon)
+        self.difficulty = difficulty
+        self.size = size
 
+    def __str__(self):
+           return f'Waypoint is {self.name} is latitude: {self.lat} longitude: {self.lon}'
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
 # YOUR CODE HERE
-
+waypoint = Waypoint('Catacombs', 41.70505, -121.51521).__str__
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
 print(waypoint)
